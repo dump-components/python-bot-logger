b91eef0b4a4e4b79e68d44d4418d1e329adfe610
Samuel Sublate <samuelsublate.dev@gmail.com>
2022-09-29 17:23:09 -0300

    refactoring class logger

--- Logger.py
+++ Logger.py
@@ -5,36 +5,35 @@ class Logger:
 
 
     def __init__(self, user: logging.Logger, syslog_host=None) -> None:
-        self._user = user
-        self._basic_config(syslog_host)
+        self.__user = user
+        self.__logger = self._basic_config(syslog_host)
 
     def info(self, message):
-        self._loggin.info(f"{self._user} {message}")
+        self.__logger.info(f"{message}")
         return message
 
     def debug(self, message):
-        self._loggin.debug(f"{self._user} {message}")
+        self.__logger.debug(f"{message}")
         return message
     
     def warning(self, message):
-        self._loggin.warning(f"{self._user} {message}")
+        self.__logger.warning(f"{message}")
         return message
 
     def error(self, message):
-        self._loggin.error(f"{self._user} {message}")
+        self.__logger.error(f"{message}")
         return message
 
     def critical(self, message):
-        self._loggin.critical(f"{self._user} {message}")
+        self.__logger.critical(f"{message}")
         return message
 
     def _basic_config(self, syslog_host: str):
-        logger = logging
-        self._loggin = logger.getLogger(self._user)
-        log_format = '%(asctime)s:%(levelname)s:%(message)s'
-        logger.basicConfig(format=log_format, level=logger.DEBUG, filename=f'./logs/bot.log', filemode='w')
+        logger = logging.getLogger(self.__user)
+        log_format = '%(message)s'
+        logging.basicConfig(format=log_format, level=logging.DEBUG, filename=f'./logs/bot.log', filemode='w')
         if syslog_host:
-            syslog_server = SysLogServer(syslog_host)
-            self._loggin.addHandler(syslog_server.graylog)
+            logger.addHandler(SysLogServer(syslog_host).graylog)
+        return logger
 
 
--- SysLogServer.py
+++ SysLogServer.py
@@ -5,8 +5,8 @@ class SysLogServer:
 
     def __init__(self, syslog_host: str) -> None:
         self.__host = syslog_host
-        self._graylog = logging.handlers.SysLogHandler(self.__host)
+        self.__graylog = logging.handlers.SysLogHandler(self.__host)
         
     @property
     def graylog(self):
-        return self._graylog
+        return self.__graylog
